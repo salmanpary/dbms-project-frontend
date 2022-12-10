209224e0b8b2f48d6b075377ed91b6b1c7cb1237
STSonyThomas <goodtobetrue2@gmail.com>
2022-12-10 20:39:29 +0530

    update:login and registration complete

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+import './App.css';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('Login');
+
+  const toggleForm = (formName)=>{
+    setCurrentForm(formName)
+  }
+
+  return (
+    <div className="App">
+      {currentForm==="Login"?<Login onFormSwitch ={toggleForm}/>:<Register onFormSwitch={toggleForm}/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React,{useState} from "react";
+
+export const Login = (props) => {
+    const [num,setNum] = useState('');
+    const [pass,setPass] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(num);
+    }
+
+    return(
+        <div className="auth-form-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+            <label htmlFor="text">Username</label>
+            <input value={num} onChange = {(e)=> setNum(e.target.value)} type="text" placeholder="Enter number"/>
+            <label htmlFor="password">Password</label>
+            <input value= {pass} onChange = {(e)=> setPass(e.target.value)} type="password" placeholder="*******"/>
+            <button className="login-btn" type ="submit" >Login</button>
+            </form>
+            <button className="under-form" onClick={()=>props.onFormSwitch('register')} >Don't have an account? Register here!</button>
+        </div>
+    )
+}
